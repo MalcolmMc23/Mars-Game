0fbf96fe387844a7ab6d11899a355a7ec21dc074
test_3 <810828@seq.org>
2022-05-06 08:43:50 -0700

    adding a frame counter to the hero class

--- hero.js
+++ hero.js
@@ -6,20 +6,30 @@ class Hero {
     this.vel = createVector(0, 0);
     this.pLevel;
     this.hCount = 0;
+    this.fCount = 0;
     this.jumpcount = 0;
   }
 
   run() {
-    this.render();
     this.update();
+    this.render();
   }
   //+++++++++++++++++++++++++++++++++++++++++Loading the hero sprite
   render() {
+    this.fCount++;
     fill(225, 20, 100);
     ellipse(this.loc.x, this.loc.y, 15, 15);
     image(heroImg[this.hCount], this.loc.x - 13, this.loc.y - 20);
     if (++this.hCount >= 5) {
       this.hCount = 0;
+    }
+    if (this.isColliding()) {
+      if (this.fCount >= 10) {
+        if (++this.hCount >= 5) {
+          this.hCount = 0;
+        }
+        this.fCount = 0;
+      }
     }
   }
   //+++++++++++++++++++++++++++++++++++++++++++++++Colliding code
@@ -43,7 +53,7 @@ class Hero {
         // this.loc.y + 7.6 < rGame.platforms[i].loc.y + 10 &&
         // this.loc.x + 7.6 > rGame.platforms[i].loc.x &&
         // this.loc.x + 7.6 < rGame.platforms[i].loc.x + 40
-        this.loc.x === 1
+        this.loc.x === this.loc.x
       ) {
         // this.pLevel = rGame.platforms[i].loc.y;
         return true;
