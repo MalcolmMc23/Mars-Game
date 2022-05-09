7ed79e1a233a938b8a5519cbb81a159daf322b73
BurberwitFries <sfrey928@gmail.com>
2022-05-09 08:32:37 -0700

    ,

--- platform.js
+++ platform.js
@@ -4,6 +4,7 @@ class Platform {
     this.acc = createVector(0, 0.1);
     this.vel = createVector(0,0);
     this.platform = []
+    this.loc = createVector(x, y);
 
   }
 
@@ -19,57 +20,9 @@ class Platform {
   }
 //++++++++++++++++++++++++++++++++++++++++++++Color and Location
   render() {
-    for (let i = 0 ; i < 16 ; i++) {
-      platform === rect + (i)
-    }
 
     fill(0, 100, 240);
-    rect0(50, 150, 40, 10);
-
-    fill(0, 100, 240);
-    rect1(100, 200, 40, 10);
-
-    fill(0, 100, 240);
-    rect2(150, 250, 40, 10);
-
-    fill(0, 100, 240);
-    rect3(200, 300, 40, 10);
-
-    fill(0, 100, 240);
-    rect4(250, 350, 40, 10);
-
-    fill(0, 100, 240);
-    rect5(300, 400, 40, 10);
-
-    fill(0, 100, 240);
-    rect6(350, 450, 40, 10);
-
-    fill(0, 100, 240);
-    rect7(400, 500, 40, 10);
-
-    fill(0, 100, 240);
-    rect8(450, 150, 40, 10);
-
-    fill(0, 100, 240);
-    rect9(500, 200, 40, 10);
-
-    fill(0, 100, 240);
-    rect10(550, 250, 40, 10);
-
-    fill(0, 100, 240);
-    rect11(600, 300, 40, 10);
-
-    fill(0, 100, 240);
-    rect12(650, 350, 40, 10);
-
-    fill(0, 100, 240);
-    rect13(700, 400, 40, 10);
-
-    fill(0, 100, 240);
-    rect14(750, 450, 40, 10);
-
-    fill(0, 100, 240);
-    rect15(800, 500, 40, 10);
+    rect(this.loc.x, this.loc.y, 40, 10);
   }
 //++++++++++++++++++++++++++++++++++++++++++Platforms move with arrow keys
   move() {
