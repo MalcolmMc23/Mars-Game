590fbfee44a196bf974e7d8d633070790349317b
BurberwitFries <sfrey928@gmail.com>
2022-05-12 08:22:08 -0700

    removed trap loc

--- trap.js
+++ trap.js
@@ -13,7 +13,7 @@ class Trap {
     triangle(x1, y1, x2, y2, x3, y3)
   }
   update() {
-    if(this.hero.loc === this.trap.loc){
+    if(this.hero.loc === this.loc){
       gameState === 3;
     }
   }
