e7256e6a9cadf7c611c3e482033447fd6bc5499b
BurberwitFries <sfrey928@gmail.com>
2022-05-12 08:09:27 -0700

    trap stuff

--- trap.js
+++ trap.js
@@ -1,11 +1,20 @@
 class Trap {
-  constructor() {}
+  constructor(x, y) {
+    this.loc = createVector(x, y)
+  }
 
   run() {
     this.render();
     this.update();
   }
 
-  render() {}
-  update() {}
+  render() {
+    fill(255,0,0);
+    triangle(x1, y1, x2, y2, x3, y3)
+  }
+  update() {
+    if(this.hero.loc === this.trap.loc){
+      gameState === 3;
+    }
+  }
 }
