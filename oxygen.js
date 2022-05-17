fa126536122f87ebe67bc2264d2e424696d7fa18
test_3 <810828@seq.org>
2022-05-17 13:07:37 -0700

    finishing oxygen (with out image)

--- oxygen.js
+++ oxygen.js
@@ -12,9 +12,32 @@ class Oxygen {
     this.update();
   }
   render() {
-    fill(0, 200, 0);
-    rect(this.loc.x, this.loc.y, this.w, this.h);
+    if (this.done === false) {
+      fill(0, 200, 0);
+      rect(this.loc.x, this.loc.y, this.w, this.h);
+    }
   }
 
-  update() {}
+  update() {
+    if (this.isGrabbing()) {
+      rGame.hero.ox = 100;
+      rGame.hero.oxFCount = 0;
+      this.done = true;
+    }
+  }
+
+  isGrabbing() {
+    //  test for collision between player and the ox
+    if (this.done === false) {
+      if (
+        rGame.hero.loc.y + rGame.hero.heroH > this.loc.y &&
+        rGame.hero.loc.y < this.loc.y + this.h + 10 &&
+        rGame.hero.loc.x + rGame.hero.heroH > this.loc.x &&
+        rGame.hero.loc.x < this.loc.x + this.w
+      ) {
+        return true;
+      }
+    }
+    return false;
+  }
 }
